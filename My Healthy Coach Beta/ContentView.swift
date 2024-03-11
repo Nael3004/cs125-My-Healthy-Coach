4e4954b1b0596348b172bc417da3150cdb3ac4d2
JohYChe <93359995+JohYChe@users.noreply.github.com>
2024-03-10 23:20:34 -0700

    Add files via upload
    Removed unnecessary connection with helper files and removed placeholders to display correct UI objeccts

--- My Healthy Coach Beta/ContentView.swift
+++ My Healthy Coach Beta/ContentView.swift
@@ -1,32 +1,151 @@
-//
-//  ContentView.swift
-//  My Healthy Coach
-//
-//  Created by Johnson Chen on 2/16/24.
-//
-
 import SwiftUI
+	
+// Schedules need to be stored on the database
+// we will extract and populate the schedule object in the app with the schedules extarcted from database
+struct Schedule {
+    let schedules: [String: [Int: String]] = [
+        // testing purpose only, see if schedule correctly displays
+        "2024-03-10": [
+            1: "Sleep",
+            2: "Sleep",
+            3: "Sleep",
+            4: "Sleep",
+            5: "Sleep",
+            6: "Sleep",
+            7: "Sleep",
+            8: "Sleep",
+            9: "Sleep",
+            10: "Eat",
+            11: "Sleep",
+            12: "Work",
+            13: "Work",
+            14: "Work",
+            15: "Work",
+            16: "Work",
+            17: "Work",
+            18: "Work",
+            19: "Work",
+            20: "Work",
+            21: "Work",
+            22: "Work",
+            23: "Work",
+            24: "Work"
+        ],
+    ]
+}
+
+struct ScheduleView: View {
+    let schedule: Schedule
+    let today: String = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: Date())
+    }()
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Today's Schedule")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                    .padding(.bottom, 10)
+                if let todaySchedule = schedule.schedules[today] {
+                    ForEach(todaySchedule.sorted(by: { $0.key < $1.key }), id: \.key) { hour, activity in
+                        HStack {
+                            Text("\(hour % 12 == 0 ? 12 : hour % 12) \(hour < 12 ? "AM" : "PM"):")
+                                .foregroundColor(.black)
+                                .font(.system(size: 23))
+                                .frame(width: 70, alignment: .leading)
+                            Text(activity)
+                                .foregroundColor(.black)
+                                .font(.system(size: 23))
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .padding(.trailing)
+                        }
+                        .padding(.horizontal)
+                    }
+                } else {
+                    Text("No schedule available for today.")
+                        .padding(.bottom, 5)
+                        .padding(.leading)
+                }
+            }
+            .padding()
+        }
+    }
+}
 
 struct ContentView: View {
     @State private var currentState: Int? = nil
-    @EnvironmentObject var tDate: Dates
+    @State private var age: Int = 1
+    @State private var dairyAllergy = false
+    @State private var fishAllergy = false
+    @State private var shellfishAllergy = false
+    @State private var nutAllergy = false
+    @State private var vegetarian = false
+
+    // UserDefaults keys
+    private let ageKey = "UserAge"
+    private let dairyKey = "dairyAllergy"
+    private let fishKey = "fishAllergy"
+    private let shellfishKey = "shellfishAllergy"
+    private let nutKey = "NutAllergy"
+    private let vegKey = "Vegetarian"
+    let schedule = Schedule()
+
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: UnitPoint.topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
             GeometryReader { geo in
                 VStack {
                     switch currentState {
                     case 0:
-                        Text("Schedule Placeholder")
+                        ScheduleView(schedule: schedule)
                     case 1:
-                        VStack(spacing: 1) {
-                            DateScroller().environmentObject(tDate).padding()
-                            dayOfWeek(for: tDate.date)
-                            CalendarGrid(for: tDate.date)
-                        }
+                        Text("Placeholder")
                     case 2:
-                        Text("Profile Placeholder")
-                    default: EmptyView()
+                        VStack {
+                            HStack {
+                                Text("Age:")
+                                    .foregroundColor(.black)
+                                    .font(.headline)
+                                Picker(selection: $age, label: Text("")) {
+                                    ForEach(1..<100) { index in Text("\(index)").tag(index)}
+                                }
+                                .pickerStyle(WheelPickerStyle())
+                                .frame(height: 100)
+                            }.padding()
+                            Toggle("Dairy Allergy", isOn: $dairyAllergy)
+                                .font(.headline).padding()
+                            Toggle("Fish Allergy", isOn: $fishAllergy)
+                                .font(.headline).padding()
+                            Toggle("Shellfish Allergy", isOn: $shellfishAllergy)
+                                .font(.headline).padding()
+                            Toggle("Nut Allergy", isOn: $nutAllergy)
+                                .font(.headline).padding()
+                            Toggle("Vegetarian", isOn: $vegetarian)
+                                .font(.headline).padding()
+                            Button(action: saveAnswers) {
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.blue)
+                                        .frame(height: 50)
+                                        .cornerRadius(10)
+                                        .padding()
+                                    Text("Save")
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .padding()
+                            .onTapGesture {
+                                withAnimation {
+                                    saveAnswers()
+                                }
+                            }
+                        }
+                    default:
+                        EmptyView()
                     }
                 }
                 .frame(width: geo.size.width, height: geo.size.height * 0.84)
@@ -46,11 +165,10 @@ struct ContentView: View {
                     Spacer()
                     Button {
                         currentState = 1
-                        tDate.date = Date()
                     } label: {
-                        Image(systemName: "calendar").resizable().cornerRadius(10).aspectRatio(contentMode:.fit).frame(width: 80, height: 80).foregroundColor(.black).padding(.horizontal)
+                        Image(systemName: "note.text").resizable().cornerRadius(10).aspectRatio(contentMode:.fit).frame(width: 80, height: 80).foregroundColor(.black).padding(.horizontal)
                     }
-                    Text("Calendar")
+                    Text("Recommendations")
                 }
                 VStack {
                     Spacer()
@@ -64,48 +182,20 @@ struct ContentView: View {
             }
         }
     }
-    func dayOfWeek(for date: Date) -> some View {
-            HStack(spacing: 1) {
-                Text("Sun").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Mon").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Tue").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Wed").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Thu").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Fri").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-                Text("Sat").dayOfW().font(.system(size: 20, weight: .bold, design: .default))
-            }
-        }
     
-    func CalendarGrid(for date: Date) -> some View {
-        VStack(spacing: 1) {
-            let daysInMonth = CalendarHelper().daysInMonth(tDate.date)
-            let firstday = CalendarHelper().firstOfMonth(tDate.date)
-            let startingSpaces = CalendarHelper().weekDay(firstday)
-            let prevM = CalendarHelper().minusMonth(tDate.date)
-            let daysInPrevMonth = CalendarHelper().daysInMonth(prevM)
-            ForEach(0..<6) {
-                row in
-                HStack(spacing: 1) {
-                    ForEach(1..<8) {
-                        column in
-                        let count = column + (row * 7)
-                        CalendarCell(count: count, startingSpaces: startingSpaces, daysInMonth: daysInMonth, daysInPrevMonth: daysInPrevMonth).environmentObject(tDate)
-                    }
-                }
-            }
-        }
-        .frame(maxHeight: .infinity)
+    // needs to connect and store answers on database
+    private func saveAnswers() {
+        UserDefaults.standard.set(age, forKey: ageKey)
+        UserDefaults.standard.set(dairyAllergy, forKey: dairyKey)
+        UserDefaults.standard.set(fishAllergy, forKey: fishKey)
+        UserDefaults.standard.set(shellfishAllergy, forKey: shellfishKey)
+        UserDefaults.standard.set(nutAllergy, forKey: nutKey)
+        UserDefaults.standard.set(vegetarian, forKey: vegKey)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-extension Text {
-    func dayOfW() -> some View {
-        self.frame(maxWidth: .infinity).padding(.top, 1).lineLimit(1)
     }
 }
